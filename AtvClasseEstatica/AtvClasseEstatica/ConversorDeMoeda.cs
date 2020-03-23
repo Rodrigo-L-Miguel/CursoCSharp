b0d618c3fd54311bd9c111bf0ce46391b3dba794
Rodrigo-L-Miguel <Rodrigo-L-Miguel@users.noreply.github.com>
2020-03-23 17:39:32 -0300

    Criação da classe principal

--- AtvClasseEstatica/AtvClasseEstatica/ConversorDeMoeda.cs
+++ AtvClasseEstatica/AtvClasseEstatica/ConversorDeMoeda.cs
@@ -6,9 +6,9 @@ namespace AtvClasseEstatica
 {
     class ConversorDeMoeda
     {
-        static double IOF = 0.06;
+        public static double IOF = 0.06;
 
-        static double ConverterDollar(double valor,double cotacao)
+        public static double ConverterDollar(double valor,double cotacao)
         {
 
             return valor * cotacao * IOF;
--- AtvClasseEstatica/AtvClasseEstatica/Program.cs
+++ AtvClasseEstatica/AtvClasseEstatica/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-
+using System.Globalization;
 namespace AtvClasseEstatica
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Qual a cotação do dollar?");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Quantos dollares você vai comprar?");
+            double valor= double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Valor a ser pago em reais: " + ConversorDeMoeda.ConverterDollar(valor,cotacao));
+
+
+
         }
     }
 }
