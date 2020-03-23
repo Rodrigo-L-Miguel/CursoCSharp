afc902da7b424f94bacbac45ffce6e0c351216b6
Rodrigo-L-Miguel <Rodrigo-L-Miguel@users.noreply.github.com>
2020-03-23 17:01:11 -0300

    Criação do projeto e classe necessária

---
+++ AtvFixacao3/AtvFixacao3/Aluno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtvFixacao3
+{
+    class Aluno
+    {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public void Aprovacao()
+        {
+            if (NotaFinal() <= 60)
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("");
+            }
+
+        }
+
+        public double NotaFinal()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+    }
+}
---
+++ AtvFixacao3/AtvFixacao3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AtvFixacao3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+            Console.WriteLine("Nome do Aluno:");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Informe as três notas do aluno:");
+            aluno.Nota1 = double.Parse(Console.ReadLine());
+            aluno.Nota2 = double.Parse(Console.ReadLine());
+            aluno.Nota3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nota final: " + aluno.NotaFinal());
+            aluno.Aprovacao();
+        }
+    }
+}
