671f4c0fbbe3061b0fdb2bf88f25b47e832173b6
Rodrigo-L-Miguel <Rodrigo-L-Miguel@users.noreply.github.com>
2020-03-23 17:33:38 -0300

    Criação do projeto e da Classe Conversorr de moeda

---
+++ AtvClasseEstatica/AtvClasseEstatica/ConversorDeMoeda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtvClasseEstatica
+{
+    class ConversorDeMoeda
+    {
+        static double IOF = 0.06;
+
+        static double ConverterDollar(double valor,double cotacao)
+        {
+
+            return valor * cotacao * IOF;
+
+        }
+
+    }
+}
