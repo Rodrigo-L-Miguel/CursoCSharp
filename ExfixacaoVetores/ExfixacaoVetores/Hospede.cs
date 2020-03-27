b073f7743b083aa88874e5c4c240231c9955255d
Rodrigo-L-Miguel <Rodrigo-L-Miguel@users.noreply.github.com>
2020-03-27 12:13:19 -0300

    Exercicio de Fixação Vetores

---
+++ ExfixacaoVetores/ExfixacaoVetores/Hospede.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExfixacaoVetores
+{
+    class Hospede
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return "Nome: " + Nome + ", E-mail: " + Email;
+        }
+    }
+}
---
+++ ExfixacaoVetores/ExfixacaoVetores/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace ExfixacaoVetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos quartos serão alugados: ");
+            int quantidadeQuartos = int.Parse(Console.ReadLine());
+
+            Hospede[] hospede = new Hospede[9];
+
+            for (int i=0; i < quantidadeQuartos; i++)
+            {
+                int j = i + 1;
+                Console.WriteLine("Hospede " + j +":");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("E-mail: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                hospede[quarto] = new Hospede { Email = email, Nome = nome };
+
+                Console.WriteLine(); 
+
+            }
+
+
+            Console.WriteLine("Quartos ocupados: ");
+            for (int i = 0; i < hospede.Length; i++)
+            {
+                if (hospede[i] != null)
+                {
+                    Console.WriteLine(i + ": " + hospede[i].ToString());
+                    Console.WriteLine();
+                }
+
+            }
+
+        }
+    }
+}
