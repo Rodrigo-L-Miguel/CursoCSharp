1132f4912d9ee38a03f64aac44df18a9b0924bb2
Rodrigo-L-Miguel <Rodrigo-L-Miguel@users.noreply.github.com>
2020-03-27 15:41:32 -0300

    Inicio atv Listas

---
+++ ExFixacaoListas/ExFixacaoListas/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExFixacaoListas
+{
+    class Funcionario
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public  double Salario { get; set; }
+
+        public Funcionario(int iD, string nome, double salario)
+        {
+            ID = iD;
+            Nome = nome;
+            Salario = salario;
+        }
+           
+        public void Aumento(int id, double porcentagem)
+        {
+            Salario += porcentagem * Salario;
+
+        }
+    
+    
+    }
+}
---
+++ ExFixacaoListas/ExFixacaoListas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExFixacaoListas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Funcionario> funcionario = new List<Funcionario>();
+
+            Console.Write("Quantos empregados serão registrados: ");
+            int totalfuncionarios = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i<totalfuncionarios; i++)
+            {
+                int j = i + 1;
+                Console.WriteLine("Funcionário: " + j );
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Salario: ");
+                double salario = double.Parse(Console.ReadLine());
+                funcionario.Add(new Funcionario(id, nome, salario));
+
+            }
+
+            Console.WriteLine(funcionario.ToString());
+        }
+    }
+}
