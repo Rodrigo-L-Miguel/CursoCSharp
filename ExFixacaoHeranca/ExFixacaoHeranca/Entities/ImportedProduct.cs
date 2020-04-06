e93a73a0ab5d6fc871412253959cb72bdb2017ca
Rodrigo-L-Miguel <Rodrigo-L-Miguel@users.noreply.github.com>
2020-04-06 15:06:18 -0300

    Initial Implementation

---
+++ ExFixacaoHeranca/ExFixacaoHeranca/Entities/ImportedProduct.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace ExFixacaoHeranca.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomnsFee { get; set; }
+
+        public ImportedProduct() { }
+
+        public ImportedProduct(string name, double price, double customnsFee): 
+            base(name,price)
+        {
+            CustomnsFee = customnsFee;
+        }
+
+        public override string PriceTag()
+        {
+            return base.PriceTag() + " (Customs fee: $ " + TotalPrice() + ")";
+
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustomnsFee;
+        }
+    }
+}
